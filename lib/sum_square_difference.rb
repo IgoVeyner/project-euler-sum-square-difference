4a9e3b4cf75e9e4af2b22b47df773e083f7694e2
Igor Veyner <igorveyner95@gmail.com>
2021-05-21 07:13:08 -0400

    add first solution

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def sum_square_difference(num)
+  current_sum_of_squares = 0
+  current_sum = 0
+
+  (1..num).each do |i|
+    current_sum_of_squares += i ** 2
+    current_sum += i
+  end
+
+  current_sum_squared = current_sum ** 2
+  current_sum_squared - current_sum_of_squares
+end
